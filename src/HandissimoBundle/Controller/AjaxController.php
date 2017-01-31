66328b0a9ce16a4cac5b81151d9643fdc59e2f79
tanguilb <tangui.lebourdonnec@gmail.com>
2017-01-31 09:33:10 +0100

    cut the research controller in two controller

--- src/HandissimoBundle/Controller/AjaxController.php
+++ src/HandissimoBundle/Controller/AjaxController.php
@@ -14,70 +14,94 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use HandissimoBundle\Form\AdvancedSearchType;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class AjaxController extends Controller
 {
+
     public function researchAction(Request $request)
     {
+
+        $em = $this->getDoctrine()->getManager();
         $form = $this->createForm('HandissimoBundle\Form\ResearchType');
         $form->handleRequest($request);
 
-        $em = $this->getDoctrine()->getManager();
-
-        $formAdvancedResearch = $this->createForm(AdvancedSearchType::class/*, $searchAdvanced, array('organizationsRepository' => ($em->getRepository('HandissimoBundle:Organizations')))  */);
-        $formAdvancedResearch->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()){
-
+        if($form->isSubmitted() && $form->isValid())
+        {
 
             $data = $form->getData();
             $age = $form->getData()['age'];
-
-            /**
-             * @var $repository OrganizationsRepository
-             */
+            $this->get('session')->set('data', $form->getData());
+            $this->get('session')->set('age', $form->getData()['age']);
             $result = $em->getRepository('HandissimoBundle:Organizations')->getByOrganizationName($data, $age);
+            $this->get('session')->set('result', $result);
             $paginator  = $this->get('knp_paginator');
-            $pagination = $paginator->paginate($result, $request->query->getInt('page', 1), 4);
-            return $this->render('front/search.html.twig', array(
-                'result' => $result,
-                'keyword' => $data,
-                'age' => $age,
-                'pagination' => $pagination,
-                'form' => $formAdvancedResearch->createView(),
-            ));
-
-        } elseif ($formAdvancedResearch->isSubmitted() && $formAdvancedResearch->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $data = $formAdvancedResearch->getData();
-            $age = $form->getData()['age'];
-
-            /**
-             * @var $repository OrganizationsRepository
-             */
-            $result = $em->getRepository('HandissimoBundle:Organizations')->getByMultipleCriterias($data, $age);
-            $paginator  = $this->get('knp_paginator');
-            $pagination = $paginator->paginate($result, $request->query->getInt('page', 1), 4);
-            return $this->render('front/search.html.twig', array(
-                'result' => $result,
-                'keyword' => $data,
-                'age' => $age,
-                'pagination' => $pagination,
-                'form' => $formAdvancedResearch->createView(),
-            ));
+            $pagination = $paginator->paginate($result, $request->query->getInt('page', 1), 2);
+            $this->get('session')->set('pagination', $pagination);
+            return $this->redirectToRoute('advanced_research_action', array('pagination' => $pagination));
         }
         return $this->render('front/index.html.twig', array(
             'form' => $form->createView(),
         ));
+
     }
 
-    public function autoCompleteAction(Request $request, $keyword)
+
+
+
+
+    public function advancedResearchAction(Request $request)
     {
-        if ($request->isXmlHttpRequest())
-        {
+        $em = $this->getDoctrine()->getManager();
+
+
+
+
+        $formAdvancedResearch = $this->createForm(AdvancedSearchType::class/*, $searchAdvanced, array('organizationsRepository' => ($em->getRepository('HandissimoBundle:Organizations')))*/  );
+        $formAdvancedResearch->handleRequest($request);
+        $session = $request->getSession();
+
+        if ($formAdvancedResearch->isSubmitted() && $formAdvancedResearch->isValid()) {
+
+            $data = $formAdvancedResearch->getData();
+            $age = $formAdvancedResearch->getData()['age'];
+
             /**
-             * @var $repository OrganizationsRepository
-             */
+            * @var $repository OrganizationsRepository
+            */
+            $result = $em->getRepository('HandissimoBundle:Organizations')->getByMultipleCriterias($data, $age);
+            $paginator  = $this->get('knp_paginator');
+            $pagination = $paginator->paginate($result, $request->query->getInt('page', 1), 2);
+            return $this->render('front/search.html.twig', array(
+              'result' => $result,
+              'keyword' =>  $data,
+              'age' => $age,
+              'pagination' => $pagination,
+              'form' => $formAdvancedResearch->createView($pagination),
+            ));
+        }
+
+          return $this->render('front/search.html.twig', array(
+              'result' => $session->get('result'),
+              'keyword' =>  $session->get('data'),
+              'age' => $session->get('age'),
+              'pagination' => $session->get('pagination'),
+              'form' => $formAdvancedResearch->createView($session->get('pagination')),
+
+          ));
+
+
+    }
+
+ 
+
+      public function autoCompleteAction(Request $request, $keyword)
+      {
+          if ($request->isXmlHttpRequest())
+          {
+              /**
+               * @var $repository OrganizationsRepository
+               */
             $repository = $this->getDoctrine()->getRepository('HandissimoBundle:Organizations');
             $organization = $repository->getByOrganizations($keyword);
 
